25ca6557b03d7d82859979764b6ebf19f939a893
sinan.uzunoglu@outlook.com <sinan.uzunoglu@outlook.com>
2021-05-28 21:09:47 +0300

    Ürünler listelendi

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
 
 @Component({
   selector: 'app-product',
@@ -6,45 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-  product1 = {
-    productId: 1,
-    productName: 'Kalem',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product2 = {
-    productId: 2,
-    productName: 'Kolonya',
-    categoryId: 2,
-    unitPrice: 15,
-  };
-  product3 = {
-    productId: 3,
-    productName: 'Limon',
-    categoryId: 3,
-    unitPrice: 25,
-  };
-  product4 = {
-    productId: 4,
-    productName: 'Mouse',
-    categoryId: 4,
-    unitPrice: 35,
-  };
-  product5 = {
-    productId: 5,
-    productName: 'Kaktüs',
-    categoryId: 5,
-    unitPrice: 45,
-  };
-  products = [
-    this.product1,
-    this.product2,
-    this.product3,
-    this.product4,
-    this.product5,
-  ];
+  products: Product[] = [];
+  apiUrl = 'https://localhost:44386/api/products/getall';
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() {}
+  ngOnInit(): void {
+    this.getProducts();
+  }
 
-  ngOnInit(): void {}
+  getProducts() {
+    this.httpClient
+      .get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        this.products = response.data;
+      });
+  }
 }
